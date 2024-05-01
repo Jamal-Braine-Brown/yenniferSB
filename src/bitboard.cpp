5cd5e9e3edf3806afca04360a2ef341620ba4c0a
Jamal Brown <53841425+Milly2CB@users.noreply.github.com>
2024-05-01 12:19:37 -0400

    Updated display function to display pieces as on bits.

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -1,16 +1,18 @@
 #include "bitboard.h"
 
 std::string Bitboards::display(Bitboard b) {
-    std::string s;
+    std::string s = "\n";
 
     for (Rank rw = RANK_8; rw >= RANK_1; --rw) {
         for (File fe = FILE_A; fe <= FILE_H; ++fe) {
             unsigned int square = rw * 8 + fe;
 
-            s += (fe == FILE_H) ? std::to_string(square) : std::to_string(square) + " ";
+            s += (!fe) ? std::to_string((int)rw + 1) : "";
+            s += (b & (1ULL << square)) ? " 1" : " 0";
         }
-        s += "\n";
+        s += "\n";  
     }
+    s += "  a b c d e f g h\n";
 
     return s;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main () {
-    Bitboard b = 0ULL;
+    Bitboard b = 15ULL;
     std::string s = Bitboards::display(b);
     std::cout << s << std::endl;
     return 0;
